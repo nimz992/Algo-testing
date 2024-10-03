587ca1717b6293e1270b765cecdee5d6d0ec1ac4
nimz992 <nimrahanwar992@gmail.com>
2024-10-02 22:40:50 -0400

    Setup Image Report

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -20,24 +20,35 @@ import "./commands";
 // require('./commands')
 
 afterEach(() => {
-    const screenshotsFolder = Cypress.config("screenshotsFolder");
-    if (window.cucumberJson?.generate) {
-      const testState = window.testState;
-      const stepResult =
-        testState.runTests[testState.currentScenario.name][testState.currentStep];
-      if (stepResult?.status === "failed") {
-        const screenshotFileName = `${testState.feature.name} -- ${testState.currentScenario.name} (failed).png`;
-        cy.readFile(
-          `${screenshotsFolder}/${Cypress.spec.name}/${screenshotFileName}`,
-          "base64"
-        ).then((imgData) => {
+  const screenshotsFolder = Cypress.config("screenshotsFolder");
+  if (window.cucumberJson?.generate) {
+    const testState = window.testState;
+    const stepResult =
+      testState.runTests[testState.currentScenario.name][testState.currentStep];
+    if (stepResult?.status === "failed") {
+      const scenarioName = testState.currentScenario.name.endsWith(".")
+        ? testState.currentScenario.name.substring(
+            0,
+            testState.currentScenario.name.length - 1
+          )
+        : testState.currentScenario.name;
+
+      const screenshotFileName = `${testState.feature.name} -- ${scenarioName} (failed).png`;
+
+      cy.wait(5000);
+      cy.readFile(
+        `${screenshotsFolder}/${Cypress.spec.name}/${screenshotFileName}`,
+        "base64"
+      ).then((imgData) => {
+        if (imgData) {
           stepResult.attachment = {
             data: imgData,
             media: { type: "image/png" },
             index: testState.currentStep,
             testCase: testState.formatTestCase(testState.currentScenario),
           };
-        });
-      }
+        }
+      });
     }
-  });
+  }
+});
